135f7c6f85ccc94a224a56612e91fbc908c131b8
Robert Rees <robert.rees@guardian.co.uk>
2015-10-27 11:36:47 +0000

    Removes the commas from the structure definition

--- thrift/storypackage.thrift
+++ thrift/storypackage.thrift
@@ -1,10 +1,10 @@
 
 struct StoryPackageItem {
-  1: required string internalPageCode,
+  1: required string internalPageCode
 }
 
 struct StoryPackage {
-  1: required string id,
-  2: required string title,
-  3: required set<StoryPackageItem> items,
+  1: required string id
+  2: required string title
+  3: required set<StoryPackageItem> items
 }
